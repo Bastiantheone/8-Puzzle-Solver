c2cef307b69d6261d3e0bb011d3154f2003ae8f7
Bastian Wieck <b.wieck@gmx.de>
2017-09-09 12:46:24 -0400

    puzzle_solver: implement A* algorithm

--- puzzle_solver/a-star.go
+++ puzzle_solver/a-star.go
@@ -1,1 +1,31 @@
 package puzzle_solver
+
+import (
+	"github.com/Bastiantheone/8-Puzzle-Solver/puzzle_solver/internal/heap"
+)
+
+func Solve(start *State) []Move {
+	var states map[string]*State
+	h := heap.New()
+	h.Push(start, 0)
+	states[start.key()] = start
+	for !h.IsEmpty() {
+		current := h.Pop()
+		if current.isGoal() {
+			return current.Moves()
+		}
+		for _, next := range current.neighbors() {
+			newCost := current.cost + 1
+			key := next.key()
+			// update if a better way to a state is found.
+			if old, exists := states[key]; !exists || newCost < old.cost {
+				next.cost = newCost
+				next.origin = current
+				states[key] = next
+				priority := newCost + next.heuristic()
+				h.Push(next, priority)
+			}
+		}
+	}
+	return nil
+}
--- puzzle_solver/game.go
+++ puzzle_solver/game.go
@@ -1,4 +1,29 @@
 package puzzle_solver
 
 type State struct {
+	origin State
+	cost   int
+}
+
+func (s *State) isGoal() bool {
+	panic("not implemented")
+}
+
+func (s *State) Moves() []Move {
+	panic("not implemented")
+}
+
+func (s *State) neighbors() []State {
+	panic("not implemented")
+}
+
+func (s *State) heuristic() int {
+	panic("not implemented")
+}
+
+func (s *State) key() string {
+	panic("not implemented")
+}
+
+type Move struct {
 }
