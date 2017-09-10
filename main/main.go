f9f6021766f06e4414bf344b41041c6a8e76dd61
Bastian Wieck <b.wieck@gmx.de>
2017-09-10 13:00:09 -0400

    main: add option for user defined goal

--- main/main.go
+++ main/main.go
@@ -8,16 +8,30 @@ import (
 )
 
 // main reads the file given as command line argument. It extracts the board from the file
-// and solves it using the A* algorithm with Manhattan distance as heuristic. The program
-// assumes the input is correct. The result is printed on the command line.
+// given as the second command line argument and solves it using the A* algorithm with Manhattan
+// distance as heuristic. The third command line argument is optional and can be a file that
+// contains the goal state. The default goal configuration is:
+// 	|0 1 2|
+// 	|6 7 8|
+//	|3 4 5|
+// The program assumes the input is correct. The result is printed on the command line.
 func main() {
 	args := os.Args
-	if len(args) != 2 {
-		panic(fmt.Errorf("puzzle_solver: got %d arguments, need 2", len(args)))
+	if len(args) != 2 || len(args) != 3 {
+		panic(fmt.Errorf("puzzle_solver: got %d arguments, need 2 or 3", len(args)))
 	}
 	board, err := puzzle_solver.Read(args[1])
 	if err != nil {
 		panic(err)
+	}
+	if len(args) == 3 {
+		goal, err := puzzle_solver.Read(args[2])
+		if err != nil {
+			panic(err)
+		}
+		puzzle_solver.SetGoalBoard(goal)
+	} else {
+		puzzle_solver.SetGoal([]int{0, 1, 2, 6, 7, 8, 3, 4, 5})
 	}
 	start := puzzle_solver.NewState(board)
 	moves := puzzle_solver.Solve(start)
